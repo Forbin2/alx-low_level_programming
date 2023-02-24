cb8729f8cca5ba2f65abf43abff46c150650cc5d
Forbin2 <forbinsky@gmail.com>
2023-02-24 07:24:32 -0800

    coding a line using c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main -print line of code using puts
+ * main -print a line of code using puts
  *
  * return: 0
 */
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzele");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
