d908b3855059469e2cc92b2fea5cc9c3a732de08
Forbin2 <forbinsky@gmail.com>
2023-02-24 23:50:23 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using main
+ *
+ * return: 0
+*/
+int main(void)
+{
+	main("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
