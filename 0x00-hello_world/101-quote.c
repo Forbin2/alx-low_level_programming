4d2efe73711b1a400536c47578b9381ec2ea9d63
Forbin2 <forbinsky@gmail.com>
2023-02-25 06:18:20 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - print a line of code using main
  *
- * return: 0
+ * Return: 1
 */
 int main(void)
 {
-	main("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	main("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
