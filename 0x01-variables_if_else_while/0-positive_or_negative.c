1e465aaa688a4c1ff913e7778979d02872cb3ea3
Forbin2 <forbinsky@gmail.com>
2023-03-03 02:09:18 -0800

    task 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
+#include <stdio.h>
+/**
+ * main - print n
+ * Return: 0
+*/
 int main(void)
 {
-	int n;
+	for (int n = 0; n < 0; n > 0; i++);
+        
+	printf("print %d,\n");
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabet character using putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet[] = a;
+	for (a <= z,
+			a++);
+		putchar("%s\n", a);
+	return (0);
+}
