5b2f0642adf4412f69b93167ef5b6c33c304146e
Forbin2 <forbinsky@gmail.com>
2023-03-05 01:23:52 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print alphabet in lower and upper case using printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet[26] = "a-Z";
+	printf("%s\n", alphabet);
+	return (0);
+}
