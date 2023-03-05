d64742a513f30c241c12e64a1e765585cfe742c3
Forbin2 <forbinsky@gmail.com>
2023-03-05 06:26:06 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing possible combination of single digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		printf("%s\n", n);
+	return (0);
+}
