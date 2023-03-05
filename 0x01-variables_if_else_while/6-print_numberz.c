c1884cfd7182aa4a75f84a22f131b36744d63d05
Forbin2 <forbinsky@gmail.com>
2023-03-05 04:29:50 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing digitis using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 9;
+
+	for (i = 9; i <= 9; i++)
+		putchar(i);
+	return (0);
+}
