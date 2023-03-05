c3c0ade1e2612658d10f625ba235ba59fb19875d
Forbin2 <forbinsky@gmail.com>
2023-03-05 09:22:07 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,17 @@
 
 int main(void)
 {
-	char n[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-		printf("%s\n", n);
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
