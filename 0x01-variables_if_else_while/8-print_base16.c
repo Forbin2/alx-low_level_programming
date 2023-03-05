656918e63300fefd74695daa6b205e55ecdbcc5d
Forbin2 <forbinsky@gmail.com>
2023-03-05 06:08:47 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print numbers of base sisteen
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i = 15;
+
+	for (i = 15; i <= 15; i++)
+		putchar (i);
+	return (0);
+}
